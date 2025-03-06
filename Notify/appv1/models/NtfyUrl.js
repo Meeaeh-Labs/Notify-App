d40a5ebfbfdf821610e47904ba5ce1f9f9e2f1b1
meeaehlabs <aehunter96@meeaeh.com>
2025-03-06 14:21:53 +0000

    Updating Beta with latest changes from app-beta

--- Notify/appv1/models/NtfyUrl.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const NtfyUrlSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    url: { type: String, required: true, unique: false }
-}, { timestamps: true });
-
-module.exports = mongoose.model('NtfyUrl', NtfyUrlSchema);
